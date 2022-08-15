7aaaa3b6e2147e24e06326a49ad86436e409ab59
Catrix <75847831+Ccccatrix@users.noreply.github.com>
2022-08-15 21:32:36 +0800

    Update Torontonian.py

--- Torontonian.py
+++ Torontonian.py
@@ -3,19 +3,16 @@ import math
 import os
 import sympy
 import matplotlib.pyplot as plt
-import multiprocessing as mp
 import scipy
-import torch
 import time
 from math import factorial
-from itertools import combinations
 from thewalrus import tor
-from tqdm import tqdm
 from numba import jit
 
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
+profix = '0815-G11'
 
 @jit(nopython=True)
 def BinToDecimal(B):
@@ -87,12 +84,21 @@ def _Prob_ABtoC(events,Probs_A,Probs_B):
         Probs_C[k] = np.sum(np.multiply(Probs_A[index_A], Probs_B[index_B]))
     return Probs_C
 
-def _A_s_tor(A,S):
-    N = len(S)
-    index_0 = np.argwhere(S)[0:]
-    index = np.vstack((index_0,index_0+N)).reshape(2*len(index_0))
-    A_S = A[index,:][:,index]
-    return A_S
+@jit(nopython=True)
+def _MD_Threshold(events,Probs):
+    D = events.shape[0]
+    M = events.shape[1]
+    C_1 = np.zeros((M,2),dtype=np.float64)
+    C_2 = np.zeros((M,M,2,2),dtype=np.float64)
+    for m in range(0,M):
+        for d in range(0,D):
+            j = events[d,m]
+            C_1[m,j] += Probs[d]
+            for n in range(0,M):
+                if n!=m:
+                    k = events[d,n]
+                    C_2[m,n,j,k] += Probs[d]
+    return  C_1, C_2
 
 class Torontonian:
 
@@ -102,9 +108,6 @@ class Torontonian:
         index = np.array([index_0,index_0+N]).reshape(-1)
         A_S = A[index,:][:,index]
         return A_S
-    #def A_s_tor(self,A,S):
-    #    res = _A_s_tor(A,S)
-    #    return res
 
     def Prob(self,sample):
         sigma_inv_s = self.A_s_tor(self.sigma_inv,sample)
@@ -162,14 +165,6 @@ class Torontonian:
 
         return self
 
-    def sigma_Squashed(self,n_mean,T):
-        sigma_in = np.zeros((2 * N, 2 * N))
-        sigma_in[0:N,0:N] = np.eye(2 * N) * n_mean
-        sigma_in[N : 2 * N,0:N] = np.eye(2 * N) * n_mean
-        sigma_in[0:N,0:N] = np.eye(2 * N) * n_mean
-        sigma_in[0:N,0:N] = np.eye(2 * N) * n_mean
-        return -1
-
     def Prob_Coherent(self,Distance,sample):
         A = np.exp( - np.sum(pow(abs(Distance), 2)))
         B = np.prod(np.array([factorial(i) for i in sample]))
@@ -187,22 +182,23 @@ class Torontonian:
     @classmethod
     def click_events(self,Z):
         return _click_events(Z)
-    
-def data():
-    dirc = os.path.abspath('.')
-    profix = '0812-F5'
 
-    names = np.load(dirc+'\\names{}.npy'.format(profix))
-    results = np.load(dirc+'\\results{}.npy'.format(profix))
+def data():
+
+    results = np.load('results{}.npy'.format(profix))
     binNum = 8
-    #print(results.shape)
+    print(results.shape)
 
     #拆分
     part_num = 0
     split_index = np.arange(0, len(results), 1, dtype = np.int32) + part_num
     results = np.array(results[split_index][:])
     Pr = np.zeros((len(results), binNum+1))
-    
+    Probs = np.zeros((len(results),256),dtype=np.float64)
+
+    for i in range(len(results)):
+        Probs[i] = results[i]/np.sum(results[i])
+
     for i in range(len(results[0])):
         phoNum = bin(i).count('1')  # 光子数
         Pr[:, phoNum] += results[:, i]
@@ -240,19 +236,30 @@ def data():
         Pr_Pi_0[i] = Pr_Pi_0[i]/np.sum(results[i])
         Pr_Pi_1[i] = Pr_Pi_1[i]/np.sum(results[i])
 
-    index = np.array(Pr[:,0]).argsort()[::-1]
+    index = np.array(Pr[:,0]).argsort()[::-1]#平均光子数从小到大排列
     Pr = Pr[index]
     Pr_Pi_0 = Pr_Pi_0[index]
     Pr_Pi_1 = Pr_Pi_1[index]
+    Probs = Probs[index]
 
-    #取后子集
+    #取子集
+    N = len(Pr)
     sub = 5
     K = 0
-    N = len(Pr)
-    Pr = Pr[N-sub-K:N-K]
-    #for i in range(len(Pr[0])):
-    #    print(Pr[0][i],', ',end = '')
-    return Pr,Pr_Pi_0
+    if sub > N:
+        sub = N
+
+    index_sub = np.arange(N-sub-K,N-K,1,dtype=np.int8)
+    Pr = Pr[index_sub]
+    Pr_Pi_0 = Pr_Pi_0[index_sub]
+    Pr_Pi_1 = Pr_Pi_1[index_sub]
+    Probs = Probs[index_sub]
+
+    np.save('Pr.npy',Pr)
+    np.save('Pr_Pi_0.npy',Pr_Pi_0)
+    np.save('Probs.npy',Probs)
+   
+    return Pr, Pr_Pi_0, Probs
 
 def getNM(g2,Np):
     n = sympy.symbols('n')
@@ -269,12 +276,16 @@ def getNM(g2,Np):
     return m,n
 
 def get_eta(n_mean,m_mean,p):
-    eta = sympy.symbols('eta')
-    fun_get_eta = p * (1+ n_mean * eta ) * (1+ m_mean * eta ) - 1
-    res = list(sympy.solveset(fun_get_eta, eta, sympy.Interval(0,1)))
-    return np.float64(res[0])
+    A = m_mean * n_mean
+    B = m_mean + n_mean
+    C = 1 - 1 / p
+    if A ==0:
+        eta = - C / B
+    else:
+        eta = (- B + math.sqrt(B ** 2 - 4 * A *C)) / (2 * A)
+    return eta
 
-def get_eta2(n_mean,m_mean,l_mean,p):
+def get_eta_3(n_mean,m_mean,l_mean,p):
     eta = sympy.symbols('eta')
     fun_get_eta = p * (1+ n_mean * eta ) * (1+ m_mean * eta ) * (1+ l_mean * eta ) - 1
     res = list(sympy.solveset(fun_get_eta, eta, sympy.Interval(0,1)))
@@ -304,16 +315,16 @@ def get_taueta(n,m,Pr):
 def get_TVD(Pr,Pr_throry):
     sum = 0
     for i in range(len(Pr)):
-        sum = sum + abs(Pr[i]-Pr_throry[i])/Pr_throry[i]
+        sum = sum + abs(Pr[i]-Pr_throry[i])#/Pr_throry[i]
     return sum
 
 @jit(nopython=True)
-def get_tau_i(a,b,c,p):
+def get_tau_i_3(a,b,c,p):
     #(1+ax)(1+bx)(1+cx)-1/p
     A = a * b * c
-    B = (a*b+b*c+c*a)
+    B = a * b + b * c + c * a
     C = a + b + c
-    D = 1-1/p
+    D = 1 - 1 / p
 
     e = 1e-6
 
@@ -324,41 +335,64 @@ def get_tau_i(a,b,c,p):
         t_1 = t_0 - ((A * t_0 ** 3 + B * t_0 ** 2 + C * t_0 + D) / (3 * A * t_0 ** 2 + 2 * B * t_0 + C))
     return t_1
 
-def get_tau(a,b,c,Pr_Pi_0):
+def get_tau_3(a,b,c,Pr_Pi_0):
     tau = np.zeros(8,dtype=np.float64)
     for i in range(8):
-        tau[i] = get_tau_i(a,b,c,Pr_Pi_0[i])
+        tau[i] = get_tau_i_3(a,b,c,Pr_Pi_0[i])
+    return tau
+
+def get_tau_i(a,b,p):
+    A = a * b
+    B = a + b
+    C = 1 - 1 / p
+    if A == 0:
+        if B!=0:
+            tau_i = - C / B
+        if B==0:
+            tau_i = 1/8
+    else:
+        tau_i = (- B + math.sqrt(B ** 2 - 4 * A *C)) / (2 * A)
+    return tau_i
+
+def get_tau(a,b,Pr_Pi_0):
+    N = len(Pr_Pi_0)
+    tau = np.zeros(N,dtype=np.float64)
+    for i in range(N):
+        tau[i] = get_tau_i(a,b,Pr_Pi_0[i])
     return tau
 
 def main(k):
+
     start=time.time()
     k_1 = 1
     k_2 = k[0]
-    k_3 = 0
 
     N = 8
     T = np.zeros((N,N))
-    Pr, Pr_Pi_0=data()
 
-    Pr_throry = np.zeros((Pr.shape[0], Pr.shape[1]))
-    Probs_C = np.zeros((Pr.shape[0], 2 ** N))
-    Probs_C2 = np.zeros((Pr.shape[0], 2 ** N))
-    P_click = 12/250
-    eta = np.zeros((5,1))
-    n_mean = 20#任意值
-    n_mean_A = k_1/(k_1+k_2+k_3) * n_mean
-    n_mean_B = k_2/(k_1+k_2+k_3) * n_mean
-    n_mean_B2 = k_3/(k_1+k_2+k_3) * n_mean
+    Pr = np.load('Pr.npy')
+    Pr_Pi_0 = np.load('Pr_Pi_0.npy')
+
+    Pr_throry = np.zeros((len(Pr), Pr.shape[1]))
+    Probs_C = np.zeros((len(Pr), 2 ** N))
+
+    n_mean_all = np.zeros(len(Pr),dtype=np.float64)
+    tau_all = np.zeros((len(Pr), N),dtype=np.float64)
+
+    C_1_Theory_all = np.zeros((len(Pr),N,2),dtype=np.float64)
+    C_2_Theory_all = np.zeros((len(Pr),N,N,2,2),dtype=np.float64)
+
+    n_mean = 10#任意值
+    n_mean_A = k_1 / (k_1 + k_2) * n_mean
+    n_mean_B = k_2 / (k_1 + k_2) * n_mean
 
     for i in range(0,len(Pr)):
 
-        eta_i = get_eta2(n_mean_A,n_mean_B,n_mean_B2,Pr[i,0])
-        #print(eta_i * n_mean_A,eta_i * n_mean_B, eta_i * n_mean_B2,Pr_Pi_0[i])
-        #print(Pr[i,0],Pr_Pi_0[i])
-        tau = get_tau(eta_i*n_mean_A, eta_i*n_mean_B, eta_i*n_mean_B2,Pr_Pi_0[i])
+        eta_i = get_eta(n_mean_A,n_mean_B,Pr[i,0])
+        tau = get_tau(eta_i*n_mean_A, eta_i*n_mean_B, Pr_Pi_0[i]).reshape(-1)
         tau = tau/np.sum(tau)
-
-        T[:,0] = np.sqrt(tau.reshape(-1))
+        tau_all[i] = tau
+        T[:,0] = np.sqrt(tau)
 
         A = Torontonian().sigma_Thermal(n_mean_A, N, T * np.sqrt(eta_i))
         B = Torontonian().sigma_Thermal(n_mean_B, N, T * np.sqrt(eta_i))
@@ -367,49 +401,106 @@ def main(k):
         B_ProbsAll,Eve = B.ProbsAll()
 
         Probs_C[i] = Torontonian.Prob_ABtoC(Eve, A_ProbsAll, B_ProbsAll)
+        C_1_Theory_all[i], C_2_Theory_all[i] = _MD_Threshold(Eve,Probs_C[i])
 
+        n_mean_all[i] = eta_i*n_mean
         print('n_mean = ',eta_i*n_mean,end=' ')
+
+    np.save('n_mean_all.npy',n_mean_all)
+    np.save('tau_all.npy',tau_all)
+    np.save('C_1_Theory_all.npy',C_1_Theory_all)
+    np.save('C_2_Theory_all.npy',C_2_Theory_all)
+    np.save('Probs_Theory_all',Probs_C)
+
     print('\n')
+
     for j in range(0,2 ** N):
         ProbNum = len(np.argwhere(Eve[j]))
         Pr_throry[:, ProbNum] += Probs_C[:, j]
+
     np.save('Pr_throry.npy',Pr_throry)
 
     Pr = Pr.reshape(-1)
     Pr_throry = Pr_throry.reshape(-1)
     TVD = get_TVD(Pr,Pr_throry)
+
     print('k=',k,'TVD = ',TVD,sep='\t',end='|\t')
+
     end=time.time()
-    print(end-start)
+    print('run time =',end - start,'s',sep=' ')
+
     return TVD                                     #TVD
 
-def test():
-    Pr, Pr_Pi_0=data()
+def TVD_plot():
+
+    Pr = np.load('Pr.npy')
     Pr_throry = np.load('Pr_throry.npy')
+    n_mean_all = np.load('n_mean_all.npy')
+    tau_all = np.load('tau_all.npy')
+    k = np.load('k.npy')
+
     delta_Pr = np.zeros((Pr.shape[0],Pr.shape[1]))
-    for i in range(len(Pr)):
-        for j in range(len(Pr_throry[0])):
-            print('%.11f' % Pr_throry[i,j],'%.11f' % Pr[i,j],'%.5f' % ((Pr_throry[i,j]-Pr[i,j])/Pr_throry[i,j]),sep = '\t')
+    print('-----------------------------------------------')
+    for i in range(Pr.shape[0]):
+        print('splitting ratio =',tau_all[i])
+        print('n_mean =',n_mean_all[i])
+        print('----------------------')
+        for j in range(Pr.shape[1]):
+            print('%.11f' % Pr_throry[i,j],'%.11f' % Pr[i,j],'%.5f' % ((Pr_throry[i,j]-Pr[i,j])/Pr_throry[i,j]), sep = '\t')
             delta_Pr[i,j] = (Pr_throry[i,j]-Pr[i,j])/Pr_throry[i,j]
         print('-----------------------------------------------')
     X = np.array([0,1,2,3,4,5,6,7,8])
-    plt.title('1')
+    plt.title(profix+'\n purity '+'%.6f'%(1/(1+k))+'(1: %.6f)'%k)
     for i in range(len(Pr)):
-        name = str(i)
-        plt.plot(X,delta_Pr[i],label=str(i))
+        plt.plot(X,delta_Pr[i],label='$\\bar{n}$=%.4f'%n_mean_all[i])
 
-    plt.legend(loc = 'lower left')
-    plt.xlabel('0~8')
+    plt.legend(loc = 'upper left')
+    plt.xlabel('$\pi_0$~$\pi_8$')
     plt.ylabel('$\Delta$')
     plt.show()
 
+def data_check():
+
+    Pr = np.load('Pr.npy')
+    Pr_Pi_0 = np.load('Pr_Pi_0.npy')
+    print('-----------------------------')
+    print('Pr')
+    print('-----------------------------')
+    print(Pr)
+    print('-----------------------------')
+    print('Pr_Pi_0')
+    print('-----------------------------')
+    print(Pr_Pi_0)
+    print('-----------------------------')
+    return 0
+
+def MD_check():
+    n_mean_all = np.load('n_mean_all.npy')
+    C_1_Theory = np.load('C_1_Theory_all.npy')
+    C_2_Theory = np.load('C_2_Theory_all.npy')
+
+    Eve = _click_events(8)
+    Probs = np.load('Probs.npy')
+    for i in range(len(Probs)):
+        C_1_i ,C_2_i = _MD_Threshold(Eve,Probs[i])
+        print('n_mean =',n_mean_all[i],'一阶边缘分布TVD =',np.sum(abs(C_1_i-C_1_Theory[i])/2),'二阶边缘分布TVD =', np.sum(abs(C_2_i-C_2_Theory[i])/2))
+
+    return 0
 
 if __name__=='__main__':
-
-    x_0 = np.array([0])
-    res_min = scipy.optimize.fmin(main,x_0,xtol = 0.0001,ftol = 1e-5)
-    x_1 = res_min
-    for i in res_min:
-        print(i)
-    main(x_1)
-    test()
+    data()#数据预处理
+    print('data check')
+    data_check()
+    print('Enter 0: Continue...','|','else: End...')
+    num = input()
+    if num == '0':
+        x_0 = np.array([0])
+        res_min = scipy.optimize.fmin(main,x_0,xtol = 0.00001,ftol = 1e-6)
+        x_1 = res_min
+        np.save('k.npy',x_1)
+        for i in res_min:
+            print(i)
+        main(x_1)
+        MD_check()
+        TVD_plot()
+        
